bbbbdfde40ad7fc85898779912000a913b113072
MykalB <mykalb081@gmail.com>
2021-07-02 18:00:12 -0400

    Routes

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,46 @@
 
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
-    
-  app.route('/api/replies/:board');
+  app.route('/api/threads/:board')
+    .post((req, res) => {
+      let { board } = req.params;
+      let {text} = req.body;
+      let { delete_password } = req.body;
 
+    })
+    .get((req, res) => {
+      let { board } = req.params;
+    })
+    .delete((req, res) => {
+      let { board } = req.params;
+      let { thread_id } = req.body;
+      let { delete_password } = req.body;
+    })
+    .put((req, res) => {
+      let { board } = req.params;
+      let { thread_id } = req.body;
+    })
+
+  app.route('/api/replies/:board')
+    .post((req, res) => {
+      let { board } = req.params;
+      let {text} = req.body;
+      let { delete_password } = req.body;
+
+    })
+    .get((req, res) => {
+      let { board } = req.params;
+      let { thread_id } = req.query;
+    })
+    .delete((req, res) => {
+      let { board } = req.params;
+      let { thread_id } = req.body;
+      let {text} = req.body;
+      let { delete_password } = req.body;
+    })
+    .put((req, res) => {
+      let { board } = req.params;
+      let { thread_id } = req.body;
+      let { reply_id } = req.body;
+    })
 };
